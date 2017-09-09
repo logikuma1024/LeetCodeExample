12e760601cca0a7b8b7d24396c932612b2807fcf
logikuma <logical.kumachan1004@gmail.com>
2017-09-09 22:06:30 +0900

    add project base

---
+++ LeetCodeExamples/LeetCodeExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
